1cfd5f8c531995a2a1b78f463cdabde23d035112
sandeepshukla1555 <90709369+sandeepshukla1555@users.noreply.github.com>
2023-09-01 18:06:44 +0530

    first.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-sm text-rose-700'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
