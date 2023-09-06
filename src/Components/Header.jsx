cc32767055fced3c0037a31810d1c86b07ab0c3f
sandeepshukla1555 <90709369+sandeepshukla1555@users.noreply.github.com>
2023-09-06 16:43:16 +0530

    Sign-in and Log-out Functionality problem fixed.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,25 @@ import header from '../img/header.svg';
 import headerMobile from '../img/headerMobile.svg';
 import logo from '../img/logo.svg';
 import { local } from '../Local';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
+    const [resumes, setResumes]=useState(null);
     let screen=window.screen.width;
     const locals=local.getItem('resume')
+    useEffect(()=>{
+      if(locals===null)
+      {
+        setResumes(null)
+      }
+      else
+      {
+        const toObject=JSON.parse(locals)
+        const resume=toObject;
+        setResumes(resume)
+      }
+    })
+    
        
   return (
     <div className='sticky -top-18'>
@@ -15,7 +30,7 @@ const Header = () => {
        <img height={100} className='top-0' src={screen<=680?headerMobile:header}/>
        </div>
        <div className='flex justify-between gap-1 items-center absolute top-3 md:top-5 w-full px-2 md:px-5'>
-        <div className='pl-0 md:pl-22 lg:pl-28'><img className='w-20 md:w-full' src={logo}/></div>
+        <div className='pl-0 md:pl-22 lg:pl-28'><img className='w-20 md:w-full' src={logo} alt={logo}/></div>
         <div className='flex justify-between items-center w-[95%] md:w-[60%]'>
             <ul className='flex md:justify-between gap-2 overflow-scroll flex-nowrap w-full md:w-[70%] scrollHeight'>
               <li><Link to='/' className='text-sm md:text-lg text-stone-50'>Home</Link></li>
@@ -25,10 +40,9 @@ const Header = () => {
                 local.removeItem('resume')
               }} className='text-sm md:text-lg text-stone-50 border-l md:border-l-0 pl-0.5'>Logout</span></li>}
             </ul>
-            <div className='flex flex-col gap-1 items-center w-[10%]'>
-              <div className='w-9 h-9 rounded-full bg-stone-800'></div>
-              <span className='hidden md:text-xs text-stone-800'>Sandeep Shukla</span>
-            </div>
+              {
+                resumes===null?<div className='flex flex-col gap-1 items-center w-[10%]'><div className='w-9 h-9 rounded-full bg-stone-800 flex justify-center items-center text-xs text-stone-50 uppercase'></div><span className='text-[5px] text-stone-800 text-center'>Not Sign-In</span></div>:<div className='flex flex-col gap-1 items-center w-[10%]'><div className='w-9 h-9 rounded-full bg-stone-800 flex justify-center items-center text-xs text-stone-50 uppercase'>{locals===null?'':`${resumes.fName[0]+"."+resumes.lName[0]+"."}`}</div><span className='text-[5px] text-stone-800 text-center'>{resumes.fName+" "+resumes.lName}</span></div>
+              }
         </div>
     </div>
     </div>
